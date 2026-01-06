61264067d5a08078cd17582bd5b74fcf802cd6f2
vinni-lacerda <vini.lacerda@hotmail.com>
2026-01-05 23:53:16 -0300

    feat: order entity created.

---
+++ OrderManagement/src/main/java/com/vinilab/OrderManagement/entities/Order.java
@@ -0,0 +1,85 @@
+package com.vinilab.OrderManagement.entities;
+import ch.qos.logback.core.net.server.Client;
+import com.vinilab.OrderManagement.enums.OrderStatus;
+import jakarta.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User user;
+
+    private OrderStatus orderStatus;
+
+    @Column(nullable = false)
+    private BigDecimal totalPrice;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    public Order() {
+    }
+
+    public Order(User user, BigDecimal totalPrice, LocalDateTime createdAt, OrderStatus orderStatus) {
+        this.user = user;
+        this.totalPrice = totalPrice;
+        this.createdAt = LocalDateTime.now();
+        this.orderStatus = OrderStatus.CREATED;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Order order)) return false;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
