0ad900a116755168d195cf61e7b3b2dbeed23fd1
vinni-lacerda <vini.lacerda@hotmail.com>
2026-01-11 20:30:53 -0300

    feat: user mapper created.

---
+++ OrderManagement/src/main/java/com/vinilab/OrderManagement/mappers/UserMapper.java
@@ -0,0 +1,32 @@
+package com.vinilab.OrderManagement.mappers;
+
+import com.vinilab.OrderManagement.dtos.UserDTO;
+import com.vinilab.OrderManagement.entities.User;
+
+public class UserMapper {
+
+    public UserMapper() {
+    }
+
+    public static User userToEntity(UserDTO dto){
+        if(dto == null){
+            throw new IllegalArgumentException("UserDTO cannot be null");
+        }
+
+        return new User(
+                dto.getName(),
+                dto.getEmail()
+        );
+    }
+
+    public static UserDTO toDTO(User entity){
+        if(entity == null){
+            throw new IllegalArgumentException("User cannot be null");
+        }
+        return new UserDTO(
+                entity.getId(),
+                entity.getName(),
+                entity.getEmail()
+        );
+    }
+}
