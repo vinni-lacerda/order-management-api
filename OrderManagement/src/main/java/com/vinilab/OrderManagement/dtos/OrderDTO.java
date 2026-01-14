42ee751d6c69fb8aa193b093b5c4372aa75db64c
vinni-lacerda <vini.lacerda@hotmail.com>
2026-01-14 01:46:08 -0300

    feat: OrderDTO created

---
+++ OrderManagement/src/main/java/com/vinilab/OrderManagement/dtos/OrderDTO.java
@@ -0,0 +1,87 @@
+package com.vinilab.OrderManagement.dtos;
+import com.vinilab.OrderManagement.enums.OrderStatus;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class OrderDTO {
+
+    private Long id;
+
+    @NotNull(message = "User id is required")
+    private Long userId;
+
+    private List<OrderItemDTO> orderItems;
+
+    @NotNull(message = "Order status is required")
+    private OrderStatus orderStatus;
+
+    @NotNull
+    @PositiveOrZero
+    private BigDecimal totalPrice;
+
+    @NotNull
+    private LocalDateTime createdAt;
+
+    public OrderDTO() {
+    }
+
+    public OrderDTO(Long id, Long userId, List<OrderItemDTO> orderItems, OrderStatus orderStatus, BigDecimal totalPrice, LocalDateTime createdAt) {
+        this.id = id;
+        this.userId = userId;
+        this.orderItems = orderItems;
+        this.orderStatus = orderStatus;
+        this.totalPrice = totalPrice;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public List<OrderItemDTO> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItemDTO> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
