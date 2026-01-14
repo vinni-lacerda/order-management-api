5ec625fa27eae741a3b4d521d07f372767378e92
vinni-lacerda <vini.lacerda@hotmail.com>
2026-01-13 23:36:05 -0300

    feat: productDTO created.

---
+++ OrderManagement/src/main/java/com/vinilab/OrderManagement/dtos/ProductDTO.java
@@ -0,0 +1,62 @@
+package com.vinilab.OrderManagement.dtos;
+
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+
+public class ProductDTO {
+    private Long id;
+
+    @NotBlank(message = "Product name is required")
+    private String name;
+
+    @NotNull(message = "Price is required")
+    @Positive(message = "Price must be greater than zero")
+    private BigDecimal price;
+
+    @NotNull
+    @PositiveOrZero(message = "Stock quantity cannot be negative")
+    private Integer stockQuantity;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(Long id, String name, BigDecimal price, Integer stockQuantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(Integer stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+}
