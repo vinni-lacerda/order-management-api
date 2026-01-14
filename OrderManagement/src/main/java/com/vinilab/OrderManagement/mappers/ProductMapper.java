6fc19de2431af35fb356d733570f234026fcbfaf
vinni-lacerda <vini.lacerda@hotmail.com>
2026-01-13 23:46:31 -0300

    feat: product mapper created.

---
+++ OrderManagement/src/main/java/com/vinilab/OrderManagement/mappers/ProductMapper.java
@@ -0,0 +1,33 @@
+package com.vinilab.OrderManagement.mappers;
+
+import com.vinilab.OrderManagement.dtos.ProductDTO;
+import com.vinilab.OrderManagement.entities.Product;
+
+public class ProductMapper {
+
+    private ProductMapper() {
+    }
+
+    public static Product productToEntity(ProductDTO dto){
+        if(dto == null){
+            throw new IllegalArgumentException("ProductDTO cannot be null");
+        }
+        return new Product(
+                dto.getName(),
+                dto.getPrice(),
+                dto.getStockQuantity()
+        );
+    }
+
+    public static ProductDTO toDTO(Product entity){
+        if(entity == null){
+            throw new IllegalArgumentException("Product cannot be null");
+        }
+        return new ProductDTO(
+                entity.getId(),
+                entity.getName(),
+                entity.getPrice(),
+                entity.getStockQuantity()
+        );
+    }
+}
