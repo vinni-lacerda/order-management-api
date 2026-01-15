1fa3bdfb677e15f9adc5f6df2177fea49a1c656c
vinni-lacerda <vini.lacerda@hotmail.com>
2026-01-15 02:03:40 -0300

    refactor: simplify OrderMapper by removing toEntity and mapping order items

--- OrderManagement/src/main/java/com/vinilab/OrderManagement/mappers/OrderMapper.java
+++ OrderManagement/src/main/java/com/vinilab/OrderManagement/mappers/OrderMapper.java
@@ -8,32 +8,17 @@ public class OrderMapper {
     public OrderMapper() {
     }
 
-    public Order toEntity(OrderDTO dto){
-        if(dto == null){
-            throw new IllegalArgumentException("OrderDTO cannot be null");
-        }
-
-        return new Order(
-                dto.getId(),
-                dto.getUserId(),
-                dto.getTotalPrice(),
-                dto.getCreatedAt(),
-                dto.getOrderItems(),
-                dto.getOrderStatus(),
-        );
-    }
-
     public OrderDTO toDTO(Order entity){
         if(entity == null){
             throw new IllegalArgumentException("Order cannot be null");
         }
         return new OrderDTO(
                 entity.getId(),
-                entity.getUser(),
-                entity.getCreatedAt(),
-                entity.getOrderItems(),
+                entity.getUser().getId(),
+                entity.getOrderItems().stream().map(OrderItemMapper::toDTO).toList(),
                 entity.getOrderStatus(),
-                entity.getTotalPrice()
+                entity.getTotalPrice(),
+                entity.getCreatedAt()
         );
     }
 }
