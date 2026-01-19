a06c87b15297cdc4a74b46a501e4aef9977a2494
vinni-lacerda <vini.lacerda@hotmail.com>
2026-01-18 23:30:08 -0300

    refactor: changing name of validator method and adding transactional annotation in class

--- OrderManagement/src/main/java/com/vinilab/OrderManagement/services/UserService.java
+++ OrderManagement/src/main/java/com/vinilab/OrderManagement/services/UserService.java
@@ -1,16 +1,16 @@
 package com.vinilab.OrderManagement.services;
-
 import com.vinilab.OrderManagement.dtos.UserDTO;
-
 import com.vinilab.OrderManagement.entities.User;
 import com.vinilab.OrderManagement.exceptions.UserNotFoundException;
 import com.vinilab.OrderManagement.mappers.UserMapper;
 import com.vinilab.OrderManagement.repositories.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class UserService {
 
     private final UserRepository userRepository;
@@ -21,16 +21,18 @@ public class UserService {
         this.userMapper = userMapper;
     }
 
+    @Transactional(readOnly = true)
     public UserDTO findById(Long id){
-        return userMapper.toDTO(findIdOrThrow(id));
+        return userMapper.toDTO(findUserOrThrow(id));
     }
 
+    @Transactional(readOnly = true)
     public List<UserDTO> findAllUsers(){
         return userRepository.findAll().stream().map(userMapper::toDTO).toList();
     }
 
     public UserDTO updateUser(Long id, UserDTO dto){
-        User user = findIdOrThrow(id);
+        User user = findUserOrThrow(id);
         user.changeName(dto.getName());
         user.changeEmail(dto.getEmail());
         User updatedUser = userRepository.save(user);
@@ -39,10 +41,11 @@ public class UserService {
     }
 
     public void deleteUser(Long id){
-        userRepository.deleteById(findIdOrThrow(id).getId());
+        userRepository.deleteById(findUserOrThrow(id).getId());
     }
 
-    public User findIdOrThrow(Long id){
+    @Transactional(readOnly = true)
+    public User findUserOrThrow(Long id){
         if(id == null){
             throw new IllegalArgumentException("User must be valid");
         }
