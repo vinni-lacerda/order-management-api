c2d3234590741ff2b527771776d97e76ecfd0b2c
vinni-lacerda <vini.lacerda@hotmail.com>
2026-01-21 01:17:39 -0300

    feat: implemented a constructor without order id

--- OrderManagement/src/main/java/com/vinilab/OrderManagement/dtos/OrderDTO.java
+++ OrderManagement/src/main/java/com/vinilab/OrderManagement/dtos/OrderDTO.java
@@ -37,6 +37,9 @@ public class OrderDTO {
         this.createdAt = createdAt;
     }
 
+    public OrderDTO(Long id, List<OrderItemDTO> list, OrderStatus orderStatus, BigDecimal totalPrice, LocalDateTime createdAt) {
+    }
+
     public Long getId() {
         return id;
     }
